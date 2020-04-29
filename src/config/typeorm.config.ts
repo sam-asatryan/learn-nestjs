9c7102b720f6b363dcf81635a7d0dcc010cc74df
Samvel Asatryan <samvel.asatryan@fouraitch.com>
2020-04-29 23:46:40 +0400

    adds loging and config variables

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,7 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config              from "config";
+
+const dbConfig = config.get("db");
 
 // export const typeOrmConfig: TypeOrmModuleOptions = {
 //     type       : 'postgres',
@@ -14,14 +17,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 // }
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type       : 'mysql',
-    host       : 'localhost',
-    port       : 3336,
-    username   : 'root',
-    password   : 'rootPassword',
-    database   : 'taskmanagement',
-    entities   : [
-        __dirname + '/../**/*.entity.{ts,js}',
-    ],
-    synchronize: true,
-}
+  type       : dbConfig.type,
+  host       : process.env.RDS_HOSTNAME || dbConfig.host,
+  port       : process.env.RDS_PORT || dbConfig.port,
+  username   : process.env.RDS_USERNAME || dbConfig.username,
+  password   : process.env.RDS_PASSWORD || dbConfig.password,
+  database   : process.env.RDS_DB_NAME || dbConfig.database,
+  entities   : [
+    __dirname + "/../**/*.entity.{ts,js}"
+  ],
+  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize
+};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule }   from './app.module'
+import { NestFactory } from "@nestjs/core";
+import { AppModule }   from "./app.module";
+import * as config     from "config";
+import { Logger }      from "@nestjs/common";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+  const logger       = new Logger("bootstrap");
+  const serverConfig = config.get("server");
+
+  const app = await NestFactory.create(AppModule);
+
+  if ("development" === process.env.NODE_ENV) {
+    app.enableCors();
+  }
+
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  logger.log(`Application is listening port ${port}`);
 }
 
 bootstrap().then(() => {
-    return
-})
+  return;
+});
